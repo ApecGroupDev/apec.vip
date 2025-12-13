fa82087ae46ee0a9fd7a6c1c0c22e479a0473f03
Mark Balajadia <markb.freelance@gmail.com>
2025-12-14 04:46:36 +0800

    2nd test push content

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -26,6 +26,7 @@ export default function Contact() {
     <div className="max-w-7xl mx-auto px-6 py-20 space-y-28">
       {/* Hero Section */}
       {/* test push content */}
+      {/* 2nd test push content */}
       <header className="relative bg-gradient-to-br from-gray-900 via-gray-800 to-black text-white text-center py-40 px-8 rounded-3xl shadow-lg overflow-hidden">
         <h1 className="text-5xl md:text-7xl font-extrabold mb-6 leading-tight">
           Get in Touch
